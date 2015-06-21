b150d3750a3585b9c02ca1146585107b22b6f714
puhrome <phirom.yim@fullsail.edu>
2015-06-21 17:21:08 -0500

    Rearranged Folders again

--- NO-Yim_Phirom_WIA_Project/js/main.js
+++ NO-Yim_Phirom_WIA_Project/js/main.js
@@ -1,38 +1,85 @@
 /**
- * Created by puhrome on 6/9/15.
+ * Created by puhrome on 6/14/15.
  */
 
-// Code for Pie Chart//
-// for skill chat jquary
-$(document).ready(function(e) {
-//var windowBottom = $(window).height();
-    var index=0;
-    $(document).scroll(function(){
-        var top = $('.skills').height()-$(window).scrollTop();
-        console.log(top)
-        if(top<-1000){
-            if(index==0){
+//FullPage.js anchors
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        sectionsColor: ["#fff","#fff", "#fff", "#fff", "#fff", "#fff"],
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastpage', 'quote'],
+        menu: '#menu',
+        scrollingSpeed: 1000
+    });
+});
+//Jquery Toggle
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").toggle();
+    });
+});
+//easy Pie Chart
 
-                $('.chart').easyPieChart({
-                    easing: 'easeOutBounce',
-                    onStep: function(from, to, percent) {
-                        $(this.el).find('.percent').text(Math.round(percent));
-                    }
-                });
+$(function() {
+    $('.chart').easyPieChart({
+    });
+});
+//html5 canvas
 
-            }
-            index++;
-        }
-    })
-//console.log(nagativeValue)
+window.onload = function() {
+//Draw images
+var theCanvas = document.getElementById('myCanvas');
+if (theCanvas && theCanvas.getContext) {
+    var ctx = theCanvas.getContext("2d");
+    if (ctx) {
+
+        //Create a variable to hold image
+        var  srcImg = document.getElementById("img1");
+
+        //Draw image
+        ctx.drawImage(srcImg, 0, -100);
+
+        //Scale image
+
+    }
+}
+//Draw text here
+// Outlined text
+var theCanvas2 = document.getElementById("myCanvas2");
+if (theCanvas2 && theCanvas2.getContext) {
+    var ctx2 = theCanvas2.getContext("2d");
+    if(ctx2) {
+        ctx2.font = "120px Damion"; //set font size and style
+        ctx2.fillStyle ="#ffffff";
+        ctx2.fillText('" Real men bake "', 330, 400); // set string, and x,y position
+    } // end of ctx5
+} // end of if for theCanvas5 context
+
+};
+
+//Jquery Ease in
+
+$( document ).click(function() {
+    $( "#toggle" ).toggle( "slide" );
 });
 
 
-// chart loding
-$(window).load(function() {
 
-    var chart = window.chart = $('.chart').data('easyPieChart');
-    $('.js_update').on('click', function() {
-        chart.update(Math.random()*100);
-    });
+//Jquery Tooltip
+
+$(".masterTooltip").hover(function() {
+    //Hover over code
+    var title = $(this).attr("title");
+    $(this).data("tipText", title).removeAttr("title");
+    $('<p class="tooltip"></p>')
+        .text(title)
+        .appendTo("body")
+        .fadeIn("slow");
+}, function() {
+    //hover out code
+    $(this).attr("title", $(this).data("tipText"));
+    $('.tooltip').remove();
+}).mousemove(function(e) {
+    var mousex = e.pageX + 100; // Get X coordinate
+    var mousey = e.pageY + 1; // Get Y coordinate
+    $('.tooltip').css({top: mousey, left: mousex})
 });
